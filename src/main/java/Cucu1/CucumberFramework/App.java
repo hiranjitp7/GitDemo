98ed01db9652eca1bf045ddc2226c586d0b2e610
hiranjit <hiranjitp7@gmail.com>
2024-08-11 23:50:59 +0530

    first commit

---
+++ src/main/java/Cucu1/CucumberFramework/App.java
@@ -0,0 +1,13 @@
+package Cucu1.CucumberFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
